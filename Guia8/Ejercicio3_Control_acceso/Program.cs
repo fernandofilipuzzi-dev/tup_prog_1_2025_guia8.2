672df02f658f60a3e9207057914acd9665681fb4
fernandofilipuzzi-dev <fernandofilipuzzi.dev@gmail.com>
2025-05-20 00:32:46 -0300

    .

--- Guia8/Ejercicio3_Control_acceso/Program.cs
+++ Guia8/Ejercicio3_Control_acceso/Program.cs
@@ -1,9 +1,4 @@
 ﻿using Ejercicio3_Control_acceso.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ejercicio3_Control_acceso
 {
